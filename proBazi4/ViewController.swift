2f21ebe0c7ebf2a8aae415e42990f68793e4be23
Mehmet Durmaz <mehmet.durmaz@temax.us>
2021-09-01 02:03:33 +0300

    19-05-13

---
+++ proBazi4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proBazi4
+//
+//  Created by Mehmet Durmaz on 12.05.2019.
+//  Copyright © 2019 Mehmet Durmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
